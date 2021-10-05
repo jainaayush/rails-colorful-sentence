55b60eb83cd930a9365a15f32c85a829a0920496
jainaayush <aayushj.pis@gmail.com>
2021-10-05 20:01:30 +0530

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sentence1 = Sentence.create(
+  description: 'Apple is looking at buying U.K. startup for $1billion'
+)
+
+Entity.create(
+  text:'Apple',
+  text_type: 'ORG',
+  sentence_id: sentence1.id
+)
+
+Entity.create(
+  text:'U.K.',
+  text_type: 'GPE',
+  sentence_id: sentence1.id
+)
+
+Entity.create(
+  text:'$1billion',
+  text_type: 'MONEY',
+  sentence_id: sentence1.id
+)
+
+sentence2 = Sentence.create(
+  description: 'Regional_funds with exposure to United_States and outperform equity_market over 3year'
+)
+
+Entity.create(
+  text:'Regional_funds',
+  text_type: 'THEME',
+  sentence_id: sentence2.id
+)
+
+Entity.create(
+  text:'United_States',
+  text_type: 'GPE',
+  sentence_id: sentence2.id
+)
+
+Entity.create(
+  text:'equity_market',
+  text_type: 'THEME',
+  sentence_id: sentence2.id
+)
+
+Entity.create(
+  text:'3year',
+  text_type: 'TIME',
+  sentence_id: sentence2.id
+)
